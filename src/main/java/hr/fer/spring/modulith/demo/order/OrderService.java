1f53a980b9b59a62f59a1f0c2481a790cfcdf45c
Mario Kusek <mario.kusek@fer.hr>
2024-08-22 20:46:13 +0200

    add using internals from nested modules - should produce test failure

--- src/main/java/hr/fer/spring/modulith/demo/order/OrderService.java
+++ src/main/java/hr/fer/spring/modulith/demo/order/OrderService.java
@@ -2,7 +2,14 @@ package hr.fer.spring.modulith.demo.order;
 
 import org.springframework.stereotype.Service;
 
+import hr.fer.spring.modulith.demo.order.slice1.Slice1;
+import hr.fer.spring.modulith.demo.order.slice2.Slice2;
+import hr.fer.spring.modulith.demo.order.slice2.internal.Slice2Service;
+
 @Service
 class OrderService implements Order {
-
+	public Slice1 slice1;
+	public Slice2 slice2;
+	
+	public Slice2Service slice2Service; // this should produce test error
 }
